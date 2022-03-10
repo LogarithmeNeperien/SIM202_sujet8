f5ab94cad6416183b33ca9b319af793211129cd6
AntoineOB <81634105+AntoineOB@users.noreply.github.com>
2022-03-10 10:13:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std ;
 
 int main()
 {   
-	char* nom_fichier = (char*)"obs3.txt";
-	const vector<Obstacle> vect_obst = reading(nom_fichier);
+    char* nom_fichier = (char*)"obs3.txt";
+    const vector<Obstacle> vect_obst = reading(nom_fichier);
     const Obstacle ob1 = vect_obst[0];
     cout << ob1 << endl;
-	double rr=0.1;
+    double rr=0.1;
     Obstacle OO=transformation_padding(ob1,rr);
     cout<<OO<<endl;
     return 0;
